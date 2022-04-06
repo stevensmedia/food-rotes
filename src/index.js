eb4fc7efb91d83a833a8fa1f2906ed95d0404f53
Neil Stevens <neil@stevensmedia.us>
2022-04-06 18:43:01 -0400

    Big fix of mistaken understanding of how the PouchDB Query API works. Additionally, change some forEach() calls to for() loops to make sure we await all asyncs when sequentially processing data

--- src/index.js
+++ src/index.js
@@ -19,35 +19,96 @@ class FoodRotes extends HTMLElement {
 <h2>Grocery List</h2>
 <p><button id="addButton">Add new item</button>
 <input id="addInput" type="text"></p>
-<p><button id="resetButton">Reset list</button>
+<p><button id="resetButton">Reset Completed Items</button>
 <ul id="list"></ul>
+<p><button id="clearButton">Clear list (Careful!)</button>
 `
 		this.addButton = this.shadow.querySelector("#addButton")
 		this.resetButton = this.shadow.querySelector("#resetButton")
+		this.clearButton = this.shadow.querySelector("#clearButton")
 		this.addInput = this.shadow.querySelector("#addInput")
 		this.addInput.placeholder = "Green eggs and ham"
 		this.list = this.shadow.querySelector("#list")
 
-		this.database = window.PouchDB('food-rotes')
+		this.database = window.PouchDB('food-rotes', {auto_compaction: true})
 
 		this.save = async () => {
-			await this.database.destroy()
-			this.database = window.PouchDB('food-rotes')
+			const items = this.list.querySelectorAll("li").entries()
+			const saveTime = Date.now()
 
-			const items = this.list.querySelectorAll("li")
-			items.forEach((item) => {
-				this.database.post({
-					type: "item",
-					caption: item.querySelector(".caption").textContent,
-					done: item.classList.contains("done")
-				})
-			})
+			for(var i = items.next(); !i.done; i = items.next()) {
+				const item = i.value[1]
+				const type = "item"
+				const caption = item.querySelector(".caption").textContent
+				const done = item.classList.contains("done")
+
+				const olddoc = await this.database.query((doc, emit) => {
+					try {
+						if(doc.type == type && doc.caption == caption) {
+							emit("idrev", true)
+						} else {
+							emit("idrev", undefined)
+						}
+					} catch(e) {
+						console.log("[SAVE] [Finding old doc] error", e)
+						console.trace()
+					}
+				}, {include_docs: true})
+
+				var puts = 0
+				for(const r in olddoc.rows) {
+					const row = olddoc.rows[r]
+					if(row.key == "idrev" && row.value) {
+						puts += 1
+						await this.database.put({
+							type: type,
+							caption: caption,
+							done: done,
+							_id: row.doc._id,
+							_rev: row.doc._rev,
+							saveTime: saveTime
+						})
+					}
+				}
+				if(!puts) {
+					await this.database.post({
+						type: type,
+						caption: caption,
+						done: done,
+						saveTime: saveTime
+					})
+				}
+			}
+
+			const unupdated = await this.database.query((doc, emit) => {
+				if(doc.type == "item" && doc.saveTime != saveTime) {
+					emit("idrev", true)
+				} else {
+					emit("idrev", false)
+				}
+			}, {include_docs: true})
+
+			for(const r in unupdated.rows) {
+				const row = unupdated.rows[r]
+				if(row.key == "idrev" && row.value) {
+					await this.database.remove(row.doc._id, row.doc._rev)
+				}
+			}
 		}
 
 		this.resetList = () => {
 			const items = this.list.querySelectorAll("li")
 			items.forEach((item) => {
 				item.classList.remove("done")
+			})
+
+			this.save()
+		}
+
+		this.clearList = () => {
+			const items = this.list.querySelectorAll("li")
+			items.forEach((item) => {
+				item.remove()
 			})
 
 			this.save()
@@ -63,7 +124,6 @@ class FoodRotes extends HTMLElement {
 				}
 			}
 
-			console.log("[appendItem]")
 			const newItem = document.createElement("li")
 			newItem.classList.add("item")
 
@@ -79,13 +139,11 @@ class FoodRotes extends HTMLElement {
 			caption.textContent = newCaption
 
 			remove.addEventListener('click', () => {
-				console.log("[Item removeButton click]")
 				newItem.remove()
 				this.save()
 			})
 
 			done.addEventListener('click', () => {
-				console.log("[Item doneButton click]")
 				if(newItem.classList.contains("done")) {
 					newItem.classList.remove("done")
 				} else {
@@ -100,32 +158,31 @@ class FoodRotes extends HTMLElement {
 
 			this.list.appendChild(newItem)
 
-			this.save()
+			if(event) {
+				this.save()
+			}
 		}
 
 		this.load = async () => {
-			console.log("[load]")
-			const res = await this.database.query(function(doc) {
-				console.log("[load query]", doc)
+			const res = await this.database.query((doc, emit) => {
 				if(doc.type == "item") {
-					emit({
-						caption: doc.caption,
-						done: doc.done
-					})
+					emit("rec", true)
+				} else {
+					emit("rec", false)
 				}
-			})
-			console.log("[load] got", res)
-			if(res && res.rows) {
-				res.rows.forEach((doc) => {
-					console.log("[load] appending", doc.key)
-					this.appendItem(undefined, doc.key.caption, doc.key.done)
-				})
+			}, {include_docs: true})
+			for(const r in res.rows) {
+				const row = res.rows[r]
+				if(row.key == "rec" && row.value) {
+					this.appendItem(undefined, row.doc.caption, row.doc.done)
+				}
 			}
 		}
 
 		this.addInput.addEventListener('change', this.appendItem)
 		this.addButton.addEventListener('click', this.appendItem)
 		this.resetButton.addEventListener('click', this.resetList)
+		this.clearButton.addEventListener('click', this.clearList)
 
 		this.load()
 	}
